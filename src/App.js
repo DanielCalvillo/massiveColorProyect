e9ab9c40ad58571c3298586e3090f1e7bbe19f79
Daniel Calvillo <danielcalvillomancilla123@gmail.com>
2020-04-01 18:51:05 -0600

    style ColorBox components

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Palette from './Components/Palette';
 function App() {
   return (
     <div className="Palette">
-      <h1>Palette</h1>
       <Palette {...seedColors[4]}/>
       {/* footer eventually */}
     </div>
