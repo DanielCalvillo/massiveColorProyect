086e8f805e399ea521071acc237cc6c55dc126da
Daniel Calvillo <danielcalvillomancilla123@gmail.com>
2020-05-25 15:27:39 -0500

    Make PaletteList responsive

--- src/Assets/sizes.js
+++ src/Assets/sizes.js
@@ -5,7 +5,8 @@ export default {
             xs: "576px",
             sm: "768px",
             md: "991.98px",
-            lg: "1199.98px"
+            lg: "1199.98px",
+            xl: "1600px"
         };
         return `@media (max-width: ${sizes[size]})`
     }
